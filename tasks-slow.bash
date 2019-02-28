729d90f86fbd4bd1cce32baaf2699ea87221153f
klemela <klemela>
2019-02-28 15:26:14 +0200

    Clean up

--- tasks-slow.bash
+++ tasks-slow.bash
@@ -9,4 +9,6 @@ date
 bash disk-read-old-file-ra256.bash 2>&1
 bash disk-read-old-file.bash 2>&1
 
+pushd java/vm-perf-mon/bin/; ./vm-perf-mon ../conf.yaml; popd
+
 popd 
--- tasks.bash
+++ tasks.bash
@@ -9,6 +9,4 @@ bash disk-write.bash
 bash net-send.bash
 bash net-receive.bash
 
-pushd java/vm-perf-mon/bin/; ./vm-perf-mon ../conf.yaml; popd
-
 popd 
