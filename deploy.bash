8848af56f1e4ac80a8956f72bbb0fc6c908111b1
Petri Klemela <petri.klemela@csc.fi>
2021-05-27 10:08:57 +0000

    Update to Ubuntu 20.04

--- deploy.bash
+++ deploy.bash
@@ -1,4 +1,1 @@
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-scp * vm0099.kaj.pouta.csc.fi:petri/vm-perf-mon/ ; ssh vm0099.kaj.pouta.csc.fi "source chipcld-openrc.sh ; cd petri/vm-perf-mon ; ansible-playbook demo.yml -e floating_ip=86.50.168.179"
+source openrc.sh ; ansible-playbook vm-perf-mon.yml -e floating_ip=FLOATING_IP
